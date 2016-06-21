ec84f3c6a883d84a5f22fbf8f30ca46097633382
sixstring982 <sixstring982@gmail.com>
2016-06-21 08:56:21 -0600

    Windowing
    Project now supports windowing, so we can draw thing now.

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,16 @@
+module Main (
+  main
+) where
+
+import Graphics.Gloss
+import Screen
+import Store
+
+window :: Display
+window = InWindow "Asteroids" Screen.dimensions Screen.position
+
+bgColor :: Color
+bgColor = black
+
 main :: IO ()
-main = putStrLn "hello, world"
+main = play window bgColor Screen.fps Store.new Store.render Store.handleEvent Store.update
---
+++ src/Screen.hs
@@ -0,0 +1,14 @@
+module Screen (
+  dimensions,
+  position,
+  fps
+) where
+
+dimensions :: (Int, Int)
+dimensions = (640, 480)
+
+position :: (Int, Int)
+position = (10, 10)
+
+fps :: Int
+fps = 60
