230d71a578a42149b3fa20fa9b61639be1a7e723
sixstring982 <sixstring982@gmail.com>
2016-06-23 09:10:56 -0600

    Vector Font
    Vector font for unsigned integers has been created.

--- src/Math.hs
+++ src/Math.hs
@@ -1,6 +1,18 @@
 module Math (
-  degFromRad
+  stot,
+  degFromRad,
+  digits
 ) where
+
+stot :: Float
+stot = (sqrt 2) / 2
 
 degFromRad :: Float -> Float
 degFromRad a = (a / pi) * 180.0
+
+digits :: Int -> [Int]
+digits n =
+  let loop n acc =
+        if n == 0 then acc
+        else loop (n `div` 10) ((n `mod` 10) : acc)
+  in loop n []
