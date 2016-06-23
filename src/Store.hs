b5e96efdb8534bcc5ed1b307ae4cf3055d84749b
sixstring982 <sixstring982@gmail.com>
2016-06-23 06:26:34 -0600

    Fragment Randomness
    Fragments generated upon death are now frame dependent, giving them
    a much more random feel.

--- src/Store.hs
+++ src/Store.hs
@@ -29,6 +29,6 @@ handleEvent e (Store n ship bs as) = Store n
                                            as
 
 update :: Float -> Store -> Store
-update f (Store n ship bs as) = Store (succ n) (Ship.update f as ship) new_bullets new_asteroids where
+update f (Store n ship bs as) = Store (succ n) (Ship.update n f as ship) new_bullets new_asteroids where
   moved_bullets = Bullet.update f bs
   (new_asteroids, new_bullets) = Asteroid.update f as moved_bullets
